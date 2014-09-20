e59af73a5cf983de6806c55447c2c613c882bded
Sebastian Porto <sebasporto@gmail.com>
2014-09-20 15:36:53 +1000

    Added tests

--- index.js
+++ index.js
@@ -2,66 +2,67 @@ var fs = require('fs');
 var path = require('path');
 
 function Plugin(options) {
-  this.options = options || {};
+	this.options = options || {};
 }
 
 Plugin.prototype.apply = function(compiler) {
-  var self = this;
+	var self = this;
 
-  compiler.plugin('emit', function(compiler, callback) {
-    var hashes = self.getHashes(compiler);
-    var outputDir = self.options.path || '.';
+	compiler.plugin('emit', function(compiler, callback) {
+		var hashes = self.getHashes(compiler);
+		var outputDir = self.options.path || '.';
 
-    // check that output folder exists
-    if (fs.lstatSync(outputDir).isDirectory()){
-      var outputFilename = self.options.filename || 'webpack-assets.json';
-      var outputFull = path.join(outputDir, outputFilename);
-      self.writeOutput(compiler, hashes, outputFull);
-      callback();
-    } else {
-      compiler.errors.push(new Error('Plugin: Folder not found ' + outputDir));
-      callback();
-    }
+		try {
+			// check that output folder exists
+			fs.lstatSync(outputDir).isDirectory();
+		} catch (e) {
+			compiler.errors.push(new Error('Plugin: Folder not found ' + outputDir));
+			return callback();
+		}
 
-  });
+		var outputFilename = self.options.filename || 'webpack-assets.json';
+		var outputFull = path.join(outputDir, outputFilename);
+		self.writeOutput(compiler, hashes, outputFull);
+		callback();
+	});
 };
 
 Plugin.prototype.writeOutput = function(compiler, hashes, outputFull) {
-  var json = JSON.stringify(hashes);
-  fs.writeFile(outputFull, json, function(err) {
-    if(err) {
-      compiler.errors.push(new Error('Plugin: Unable to save to ' + outputFull));
-    }
-  }); 
-  // compiler.assets[outputFilename] = {
-  //  source: function() {
-  //    return json;
-  //  },
-  //  size: function() {
-  //    return json.length;
-  //  }
-  // };
+	var json = JSON.stringify(hashes);
+	fs.writeFile(outputFull, json, function(err) {
+		if (err) {
+			compiler.errors.push(new Error('Plugin: Unable to save to ' + outputFull));
+		}
+	});
+	// compiler.assets[outputFilename] = {
+	//  source: function() {
+	//    return json;
+	//  },
+	//  size: function() {
+	//    return json.length;
+	//  }
+	// };
 };
 
 Plugin.prototype.getHashes = function(compiler) {
-  var webpackStatsJson = compiler.getStats().toJson();
-  var assets = {};
-  for (var chunk in webpackStatsJson.assetsByChunkName) {
-    var chunkValue = webpackStatsJson.assetsByChunkName[chunk];
+	var webpackStatsJson = compiler.getStats().toJson();
+	var assets = {};
+	for (var chunk in webpackStatsJson.assetsByChunkName) {
+		var chunkValue = webpackStatsJson.assetsByChunkName[chunk];
 
-    // Webpack outputs an array for each chunk when using sourcemaps
-    if (chunkValue instanceof Array) {
-      // Is the main bundle always the first element?
-      chunkValue = chunkValue[0];
-    }
+		// Webpack outputs an array for each chunk when using sourcemaps
+		if (chunkValue instanceof Array) {
+			// Is the main bundle always the first element?
+			chunkValue = chunkValue[0];
+		}
 
-    if (compiler.options.output.publicPath) {
-      chunkValue = compiler.options.output.publicPath + chunkValue;
-    }
-    assets[chunk] = chunkValue;
-  }
+		if (compiler.options.output.publicPath) {
+			chunkValue = compiler.options.output.publicPath + chunkValue;
+		}
+		assets[chunk] = chunkValue;
+	}
 
-  return assets;
+	return assets;
 };
 
 module.exports = Plugin;
--- spec/index_spec.js
+++ spec/index_spec.js
@@ -8,234 +8,149 @@ var Plugin = require('../index.js');
 var OUTPUT_DIR = path.join(__dirname, '../dist');
 
 function testPlugin(webpackConfig, expectedResults, outputFile, done) {
-  // Create output folder
-  mkdirp(OUTPUT_DIR, function(err) { 
-    expect(err).toBeFalsy();
+	// Create output folder
+	mkdirp(OUTPUT_DIR, function(err) {
+		expect(err).toBeFalsy();
 
-    outputFile = outputFile || 'webpack-assets.json';
+		outputFile = outputFile || 'webpack-assets.json';
 
-    webpack(webpackConfig, function(err, stats) {
-      expect(err).toBeFalsy();
-      expect(stats.hasErrors()).toBe(false);
-      
-      var content = fs.readFileSync(path.join(OUTPUT_DIR, outputFile)).toString();
+		webpack(webpackConfig, function(err, stats) {
+			expect(err).toBeFalsy();
+			expect(stats.hasErrors()).toBe(false);
 
-      for (var i = 0; i < expectedResults.length; i++) {
-        var expectedResult = expectedResults[i];
-        if (expectedResult instanceof RegExp) {
-          expect(content).toMatch(expectedResult);
-        } else {
-          expect(content).toContain(expectedResult);
-        }
-      }
-      done();
-    });
+			var content = fs.readFileSync(path.join(OUTPUT_DIR, outputFile)).toString();
 
-  });
+			for (var i = 0; i < expectedResults.length; i++) {
+				var expectedResult = expectedResults[i];
+				if (expectedResult instanceof RegExp) {
+					expect(content).toMatch(expectedResult);
+				} else {
+					expect(content).toContain(expectedResult);
+				}
+			}
+			done();
+		});
+
+	});
 }
 
 describe('Plugin', function() {
-  beforeEach(function(done) {
-    rm_rf(OUTPUT_DIR, done);
-  });
+	beforeEach(function(done) {
+		rm_rf(OUTPUT_DIR, done);
+	});
 
-  it('generates a default webpack-assets.json file for a single entry point', function(done) {
-    var webpackConfig = {
-      entry: path.join(__dirname, 'fixtures/one.js'),
-      output: {
-        path: OUTPUT_DIR,
-        filename: 'index_bundle.js'
-      },
-      plugins: [new Plugin({path: 'dist'})]
-    };
+	it('generates a default file for a single entry point', function(done) {
+		var webpackConfig = {
+			entry: path.join(__dirname, 'fixtures/one.js'),
+			output: {
+				path: OUTPUT_DIR,
+				filename: 'index-bundle.js'
+			},
+			plugins: [new Plugin({
+				path: 'dist'
+			})]
+		};
 
-    testPlugin(webpackConfig, ['{"main":"index_bundle.js"}'], null, done);
+		var expected = ['{"main":"index-bundle.js"}'];
 
-  });
+		testPlugin(webpackConfig, expected, null, done);
+	});
 
-  // it('generates a default index.html file with multiple entry points', function(done) {
-  //   testPlugin({
-  //     entry: {
-  //       util: path.join(__dirname, 'fixtures/util.js'),
-  //       app: path.join(__dirname, 'fixtures/index.js')
-  //     },
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: '[name]_bundle.js'
-  //     },
-  //     plugins: [new Plugin()]
-  //   }, ['<script src="util_bundle.js"', '<script src="app_bundle.js"'], null, done);
-  // });
+	it('generates a default file with multiple entry points', function(done) {
+		var webpackConfig = {
+			entry: {
+				one: path.join(__dirname, 'fixtures/one.js'),
+				two: path.join(__dirname, 'fixtures/two.js')
+			},
+			output: {
+				path: OUTPUT_DIR,
+				filename: '[name]-bundle.js'
+			},
+			plugins: [new Plugin({
+				path: 'dist'
+			})]
+		};
 
-  // it('allows you to specify your own HTML template file', function(done) {
-  //   testPlugin({
-  //     entry: {
-  //       app: path.join(__dirname, 'fixtures/index.js')
-  //     },
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: '[name]_bundle.js'
-  //     },
-  //     plugins: [new Plugin({template: path.join(__dirname, 'fixtures/test.html')})]
-  //   },
-  //   ['<script src="app_bundle.js"', 'Some unique text'], null, done);
-  // });
+		var expected = ['{"one":"one-bundle.js","two":"two-bundle.js"}'];
 
-  // it('allows you to specify your own HTML template string', function(done) {
-  //   testPlugin({
-  //     entry: {app: path.join(__dirname, 'fixtures/index.js')},
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'app_bundle.js'
-  //     },
-  //     plugins: [new Plugin({
-  //       templateContent: fs.readFileSync(path.join(__dirname, 'fixtures/test.html'), 'utf8')
-  //     })]
-  //   },
-  //   ['<script src="app_bundle.js"'], null, done);
-  // });
+		testPlugin(webpackConfig, expected, null, done);
+	});
 
-  // it('registers a webpack error both template and template content are specified', function(done) {
-  //   webpack({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle.js'
-  //     },
-  //     plugins: [new Plugin({
-  //       template: path.join(__dirname, 'fixtures/test.html'),
-  //       templateContent: 'whatever'
-  //     })]
-  //   }, function(err, stats) {
-  //     expect(stats.hasErrors()).toBe(true);
-  //     expect(stats.toJson().errors[0]).toContain('Plugin');
-  //     done();
-  //   });
-  // });
+	it('allows you to specify your own filename', function(done) {
 
-  // it('works with source maps', function(done) {
-  //   testPlugin({
-  //     devtool: 'sourcemap',
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle.js'
-  //     },
-  //     plugins: [new Plugin()]
-  //   }, ['<script src="index_bundle.js"'], null, done);
-  // });
+		var webpackConfig = {
+			entry: path.join(__dirname, 'fixtures/one.js'),
+			output: {
+				path: OUTPUT_DIR,
+				filename: 'index-bundle.js'
+			},
+			plugins: [new Plugin({
+				filename: 'foo.json',
+				path: 'dist'
+			})]
+		};
 
-  // it('handles hashes in bundle filenames', function(done) {
-  //   testPlugin({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle_[hash].js'
-  //     },
-  //     plugins: [new Plugin()]
-  //   }, [/<script src="index_bundle_[0-9a-f]+\.js"/], null, done);
-  // });
+		var expected = ['{"main":"index-bundle.js"}'];
 
-  // it('prepends the webpack public path to script src', function(done) {
-  //   testPlugin({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle.js',
-  //       publicPath: 'http://cdn.example.com/assets/'
-  //     },
-  //     plugins: [new Plugin()]
-  //   }, ['<script src="http://cdn.example.com/assets/index_bundle.js"'], null, done);
-  // });
+		testPlugin(webpackConfig, expected, 'foo.json', done);
 
-  // it('handles subdirectories in the webpack output bundles', function(done) {
-  //   testPlugin({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'assets/index_bundle.js'
-  //     },
-  //     plugins: [new Plugin()]
-  //   }, ['<script src="assets/index_bundle.js"'], null, done);
-  // });
+	});
 
-  // it('handles subdirectories in the webpack output bundles along with a public path', function(done) {
-  //   testPlugin({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'assets/index_bundle.js',
-  //       publicPath: 'http://cdn.example.com/'
-  //     },
-  //     plugins: [new Plugin()]
-  //   }, ['<script src="http://cdn.example.com/assets/index_bundle.js"'], null, done);
-  // });
 
-  // it('allows you to configure the title of the generated HTML page', function(done) {
-  //   testPlugin({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle.js'
-  //     },
-  //     plugins: [new Plugin({title: 'My Cool App'})]
-  //   }, ['<title>My Cool App</title>'], null, done);
-  // });
+	it('registers a webpack error when output folder doesnt exists', function(done) {
+		var webpackConfig = {
+			entry: path.join(__dirname, 'fixtures/one.js'),
+			output: {
+				path: OUTPUT_DIR,
+				filename: 'index-bundle.js'
+			},
+			plugins: [new Plugin({
+				path: 'notFound'
+			})]
+		};
 
-  // it('allows you to configure the output filename', function(done) {
-  //   testPlugin({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle.js'
-  //     },
-  //     plugins: [new Plugin({filename: 'test.html'})]
-  //   }, ['<script src="index_bundle.js"'], 'test.html', done);
-  // });
+		webpack(webpackConfig, function(err, stats) {
+			expect(stats.hasErrors()).toBe(true);
+			expect(stats.toJson().errors[0]).toContain('Plugin');
+			done();
+		});
+	});
 
-  // it('allows you to configure the output filename with a path', function(done) {
-  //   testPlugin({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle.js'
-  //     },
-  //     plugins: [new Plugin({filename: 'assets/test.html'})]
-  //   }, ['<script src="index_bundle.js"'], 'assets/test.html', done);
-  // });
+	it('works with source maps', function(done) {
 
-  // it('allows you write multiple HTML files', function(done) {
-  //   testPlugin({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle.js'
-  //     },
-  //     plugins: [
-  //       new Plugin(),
-  //       new Plugin({
-  //         filename: 'test.html',
-  //         template: path.join(__dirname, 'fixtures/test.html')
-  //       })
-  //     ]
-  //   }, ['<script src="index_bundle.js"'], null, done);
+		var webpackConfig = {
+			devtool: 'sourcemap',
+			entry: path.join(__dirname, 'fixtures/one.js'),
+			output: {
+				path: OUTPUT_DIR,
+				filename: 'index-bundle.js'
+			},
+			plugins: [new Plugin({
+				path: 'dist'
+			})]
+		};
 
-  //   expect(fs.existsSync(path.join(__dirname, 'fixtures/test.html'))).toBe(true);
-  // });
+		var expected = ['{"main":"index-bundle.js"}'];
 
-  // it('registers a webpack error if the template cannot be opened', function(done) {
-  //   webpack({
-  //     entry: path.join(__dirname, 'fixtures/index.js'),
-  //     output: {
-  //       path: OUTPUT_DIR,
-  //       filename: 'index_bundle.js'
-  //     },
-  //     plugins: [new Plugin({template: 'fixtures/does_not_exist.html'})]
-  //   }, function(err, stats) {
-  //     expect(stats.hasErrors()).toBe(true);
-  //     expect(stats.toJson().errors[0]).toContain('Plugin');
-  //     done();
-  //   });
-  // });
+		testPlugin(webpackConfig, expected, null, done);
+	});
+
+	it('handles hashes in bundle filenames', function(done) {
+
+		var webpackConfig = {
+			entry: path.join(__dirname, 'fixtures/one.js'),
+			output: {
+				path: OUTPUT_DIR,
+				filename: 'index-bundle-[hash].js'
+			},
+			plugins: [new Plugin({
+				path: 'dist'
+			})]
+		};
+
+		var expected = [/{"main":"index-bundle-[0-9a-f]+\.js"}/];
+
+		testPlugin(webpackConfig, expected, null, done);
+	});
 
 });
