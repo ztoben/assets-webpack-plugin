15f70dd94d55a6d9b7745da88f12f54fd04feec1
ju1ius <jules.bernable@laposte.net>
2015-10-07 09:38:33 +0200

    added tests for the update config option

--- test/multiCompiler.test.js
+++ test/multiCompiler.test.js
@@ -1,27 +1,23 @@
 /*jshint expr: true*/
 
 var path = require('path');
-// var mocha = require('mocha');
-var chai = require('chai');
+var expect = require('chai').expect;
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var rm_rf = require('rimraf');
-var mkdirp = require('mkdirp');
-var _ = require('lodash');
+
 var Plugin = require('../index.js');
-var expect = chai.expect;
 
 var OUTPUT_DIR = path.join(__dirname, '../tmp');
 var expectOutput = require('./utils/expectOutput')(OUTPUT_DIR);
 
 
-describe('Plugin with multi-compiler', function() {
+describe('Plugin', function () {
 
-    beforeEach(function(done) {
+    beforeEach(function (done) {
         rm_rf(OUTPUT_DIR, done);
     });
 
-    it('generates a default file with multiple compilers', function(done) {
+    it('works in multi-compiler mode', function (done) {
         var plugin = new Plugin({path: 'tmp'});
 
         var webpackConfig = [
@@ -49,10 +45,10 @@ describe('Plugin with multi-compiler', function() {
 
         var expected = {
             one: {
-                js: "one-bundle.js"
+                js: 'one-bundle.js'
             },
             two: {
-                js: "two-bundle.js"
+                js: 'two-bundle.js'
             }
         };
 
@@ -62,5 +58,69 @@ describe('Plugin with multi-compiler', function() {
         };
 
         expectOutput(args, done);
+    });
+
+    it('updates output between compiler calls when options.update is true', function (done) {
+        var config_1 = {
+            entry: {
+                one: path.join(__dirname, 'fixtures/one.js')
+            },
+            output: {
+                path: OUTPUT_DIR,
+                filename: 'one-bundle.js'
+            },
+            plugins: [new Plugin({path: 'tmp', update: true})]
+        };
+        var config_2 = {
+            entry: {
+                two: path.join(__dirname, 'fixtures/two.js')
+            },
+            output: {
+                path: OUTPUT_DIR,
+                filename: 'two-bundle.js'
+            },
+            plugins: [new Plugin({path: 'tmp', update: true})]
+        };
+
+        var expected = {one: {js: 'one-bundle.js'}, two: {js: 'two-bundle.js'}};
+        var args = {config: config_2, expected: expected};
+
+        webpack(config_1, function (err, stats) {
+            expect(err).to.be.null;
+            expect(stats.hasErrors()).to.be.false;
+            expectOutput(args, done);
+        });
+    });
+
+    it('overwrites output between compiler calls when options.update is false', function (done) {
+        var config_1 = {
+            entry: {
+                one: path.join(__dirname, 'fixtures/one.js')
+            },
+            output: {
+                path: OUTPUT_DIR,
+                filename: 'one-bundle.js'
+            },
+            plugins: [new Plugin({path: 'tmp', update: false})]
+        };
+        var config_2 = {
+            entry: {
+                two: path.join(__dirname, 'fixtures/two.js')
+            },
+            output: {
+                path: OUTPUT_DIR,
+                filename: 'two-bundle.js'
+            },
+            plugins: [new Plugin({path: 'tmp', update: false})]
+        };
+
+        var expected = {two: {js: 'two-bundle.js'}};
+        var args = {config: config_2, expected: expected};
+
+        webpack(config_1, function (err, stats) {
+            expect(err).to.be.null;
+            expect(stats.hasErrors()).to.be.false;
+            expectOutput(args, done);
+        });
     });
 });
