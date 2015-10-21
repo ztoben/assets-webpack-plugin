c878ecc617ed85b71ad2383d078597fd5348d4b7
Ernesto Rocha <ernestorrocha@gmail.com>
2015-10-21 16:51:55 -0300

    Adds the flag to not inlude the publicPath

--- test/getAssetKind.test.js
+++ test/getAssetKind.test.js
@@ -8,27 +8,27 @@ describe('getAssetKind', function () {
 
   beforeEach(function () {
     webpackConfig = {
-        output: {
-            filename: '[name].js?[hash]',
-            sourceMapFilename: '[file].map[query]'
-          },
-        devtool: 'sourcemap'
-      };
+      output: {
+          filename: '[name].js?[hash]',
+          sourceMapFilename: '[file].map[query]'
+        },
+      devtool: 'sourcemap'
+    };
   });
 
   describe('js', function () {
 
     it('returns js', function () {
-        var input = 'desktop.js';
-        var res = getAssetKind(webpackConfig, input);
-        expect(res).to.eq('js');
-      });
+      var input = 'desktop.js';
+      var res = getAssetKind(webpackConfig, input);
+      expect(res).to.eq('js');
+    });
 
     it('returns js with hash', function () {
-        var input = 'desktop.js?9b913c8594ce98e06b21';
-        var res = getAssetKind(webpackConfig, input);
-        expect(res).to.eq('js');
-      });
+      var input = 'desktop.js?9b913c8594ce98e06b21';
+      var res = getAssetKind(webpackConfig, input);
+      expect(res).to.eq('js');
+    });
 
   });
 
