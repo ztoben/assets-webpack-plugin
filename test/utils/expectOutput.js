b95df5e30eb2b2a49ea808c0939e76a22d8a69a5
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2020-06-08 09:46:41 +0200

    Upgrade mkdirp to 1.0.4 following deprecation prompt
    > Legacy versions of mkdirp are no longer supported. Please update to mkdirp 1.x.
    > (Note that the API surface has changed to use Promises in 1.x.)
    
    Closes #226

--- test/utils/expectOutput.js
+++ test/utils/expectOutput.js
@@ -24,29 +24,33 @@ module.exports = function (outputDir) {
     var outputFile = args.outputFile
 
     // Create output folder
-    mkdirp(outputDir, function (err) {
-      expect(err).to.be.null
-
-      outputFile = outputFile || 'webpack-assets.json'
-
-      webpack(webpackConfig, function (err, stats) {
-        expect(err).to.be.null
-        expect(stats.hasErrors()).to.be.false
-
-        var content = fs.readFileSync(path.join(outputDir, outputFile)).toString()
-
-        if (_.isRegExp(expectedResult)) {
-          expect(content).to.match(expectedResult)
-        } else if (_.isString(expectedResult)) {
-          expect(content).to.contain(expectedResult)
-        } else {
-          // JSON object provided
-          var actual = JSON.parse(content)
-          expect(actual).to.eql(expectedResult)
-        }
-
-        done()
+    mkdirp(outputDir)
+      .then(function () {
+        return null
       })
-    })
+      .then(function (err) {
+        expect(err).to.be.null
+
+        outputFile = outputFile || 'webpack-assets.json'
+
+        webpack(webpackConfig, function (err, stats) {
+          expect(err).to.be.null
+          expect(stats.hasErrors()).to.be.false
+
+          var content = fs.readFileSync(path.join(outputDir, outputFile)).toString()
+
+          if (_.isRegExp(expectedResult)) {
+            expect(content).to.match(expectedResult)
+          } else if (_.isString(expectedResult)) {
+            expect(content).to.contain(expectedResult)
+          } else {
+            // JSON object provided
+            var actual = JSON.parse(content)
+            expect(actual).to.eql(expectedResult)
+          }
+
+          done()
+        })
+      })
   }
 }
